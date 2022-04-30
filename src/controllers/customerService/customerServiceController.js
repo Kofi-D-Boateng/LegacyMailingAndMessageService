ea18a61f8b26c4f336c9fe6e98b68dd1eb56a234
Kofi Boateng <kdboat2@gmail.com>
2022-04-29 22:32:53 -0500

    Enabled json rendering. Linked frontend to mailer.

--- src/controllers/customerService/customerServiceController.js
+++ src/controllers/customerService/customerServiceController.js
@@ -1,3 +1,9 @@
-const service = async (req, res) => {};
+const service = async (req, res) => {
+  const DATA = {
+    emailer: req.body.email,
+    msg: req.body.text,
+  };
+  console.log(DATA);
+};
 
 module.exports = { service };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 "use strict";
 require("dotenv").config();
-const app = require("express")();
+const express = require("express");
+const app = express();
 const logger = require("morgan");
 const PORT = process.env.ENVIRONMENT === "prod" ? undefined : 5500;
 
@@ -13,14 +14,17 @@ const CUSTOMERSERVICE = require("../src/routes/customerService");
 
 // WHITELIST
 const WHITELIST = {
-  origin: process.env.ORIGINS,
+  origins: "*" || process.env.ORIGINS,
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   optionSuccessStatus: 200,
 };
 
 //MIDDLEWARE
+app.use(logger("dev"));
+app.use(express.json());
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", `${WHITELIST.origin}`);
+  res.header("Access-Control-Allow-Origin", `${WHITELIST.origins}`);
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.header(
     "Access-Control-Allow-Headers",
@@ -28,7 +32,6 @@ app.use((req, res, next) => {
   );
   next();
 });
-app.use(logger("dev"));
 
 // ROUTES
 app.use("/user/notifications", NOTIFICATIONS);
