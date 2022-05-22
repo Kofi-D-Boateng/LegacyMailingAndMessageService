3010d5a1cdc89173d3081269d6fb03dce32640a3
Kofi Boateng <kdboat2@gmail.com>
2022-05-22 11:12:08 -0500

    Added route to send new verification email when token expires.

--- src/routes/verification/verification.js
+++ src/routes/verification/verification.js
@@ -4,5 +4,6 @@ import sendVerificationEmail from "../../controllers/verification/verficationCon
 const router = express.Router();
 
 router.post("/send-email", sendVerificationEmail);
+router.post("/new-verification-link", sendVerificationEmail);
 
 export default router;
